6a08f5284abbd82b37948bc6af81dee7a599b90a
Jan Raadts <janraadts@outlook.de>
2022-10-30 17:59:01 +0100

    add some style and sorted the css files in underfolders

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+// Variablen aus dem Formular
+
 const anzahlPersonen = 3;
 const anzahlKm = 180;
 const preisProLiter = 1.9;
 const verbrauchDurchschnitt = 9;
+
+// Funktion zur Sprikostenberechnung
 
 function berechnung() {
   const result =
@@ -9,4 +13,11 @@ function berechnung() {
   return result;
 }
 
-console.log(berechnung());
+// Variable aus dem button und variable für den Output
+let button = document.querySelector(".berechnen_btn");
+const output = document.querySelector('[js-data="output"]');
+
+// Button Click funktion ausführung der funktion und ausgabe in der Konsole
+button.addEventListener("click", () => {
+  console.log(berechnung());
+});
